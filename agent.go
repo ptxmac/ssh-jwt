e7e1d4cd249afa07ede234ead20268bbacf94524
Peter Kristensen <git@ptx.dk>
2019-11-30 19:29:38 +0000

    Scaffold

---
+++ agent.go
@@ -0,0 +1,53 @@
+package sshjwt
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	sshagent "golang.org/x/crypto/ssh/agent"
+)
+
+type Agent interface {
+	FirstKey() (*keyWrapper, error)
+	AllKeys() ([]*keyWrapper, error)
+}
+
+type agent struct {
+	client sshagent.ExtendedAgent
+}
+
+func (a *agent) AllKeys() ([]*keyWrapper, error) {
+	keys, err := a.client.List()
+	if err != nil {
+		return nil, err
+	}
+	var res []*keyWrapper
+	for _, key := range keys {
+		res = append(res, a.wrapKey(key))
+	}
+	return res, nil
+}
+
+func (a *agent) FirstKey() (*keyWrapper, error) {
+	keys, err := a.client.List()
+	if err != nil {
+		return nil, err
+	}
+	if len(keys) == 0 {
+		return nil, fmt.Errorf("no keys found")
+	}
+	return a.wrapKey(keys[0]), nil
+}
+
+func (a *agent) wrapKey(key *sshagent.Key) *keyWrapper {
+	return &keyWrapper{
+		agent:  a,
+		pubKey: key,
+	}
+}
+
+func DefaultAgent() (Agent, error) {
+	conn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+	return &agent{client: sshagent.NewClient(conn)}, err
+}
---
+++ cmd/ssh-jwt/verify_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"testing"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/ssh/agent"
+
+	sshjwt "github.com/ptxmac/ssh-jwt"
+)
+
+func TestVerify(t *testing.T) {
+	ring := sshjwt.NewKeyring()
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(t, err)
+	require.NoError(t, ring.AddKey(agent.AddedKey{PrivateKey: priv}))
+
+	getDefaultAgent = func() (agent sshjwt.Agent, err error) {
+		return ring, nil
+	}
+
+	token := jwt.NewWithClaims(sshjwt.SSHSigningMethod, nil)
+	key, err := ring.FirstKey()
+	require.NoError(t, err)
+	str, err := token.SignedString(key)
+	require.NoError(t, err)
+
+	err = verifyCmd(nil, []string{str})
+	assert.NoError(t, err)
+}
